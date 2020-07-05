b6d65d487dd5c8ba74cd8023baa6c7add3200278
Christoph Schüler <christoph.schueler@googlemail.com>
2020-07-05 16:52:32 +0200

    Change table structure
    No more date table needed

--- data/sales.sql
+++ data/sales.sql
@@ -1,15 +1,9 @@
-CREATE TABLE date(
-    dateID SERIAL PRIMARY KEY,
+CREATE TABLE sales(
+    saleID SERIAL PRIMARY KEY,
     day int NOT NULL,
     month int NOT NULL,
     year int NOT NULL,
-    quarter int NOT NULL
-);
-
-
-CREATE TABLE sales(
-    saleID SERIAL PRIMARY KEY,
-    date int REFERENCES date (dateID),
+    quarter int NOT NULL,
     shopID int REFERENCES shop (shopid),
     articleID int REFERENCES article (articleid),
     sold int NOT NULL,
